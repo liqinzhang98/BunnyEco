dcce29bdddc43655f53c3a187bf125bcb323d6f0
liqinzhang98 <103245879+liqinzhang98@users.noreply.github.com>
2024-05-22 21:46:49 +1000

    updated navi bar

--- src/customer/Home.js
+++ src/customer/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from './Navbar';
 
 const Home = () => {
     return (
         <div>
+            <Navbar />
             <h1>Home Page1</h1>    
         </div>
     )
