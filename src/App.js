075ca34db3aed45d20d8adbc75524b81b599accb
liqinzhang98 <liqizhang@student.unimelb.edu.au>
2024-05-22 16:21:31 +1000

    updated pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import {Switch, Route} from 'react-router-dom';
+import Home from './customer/Home';
+import Login from './customer/Login';
+import Register from './customer/Register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route exact path='/'><Home /></Route>
+        <Route path='/login'><Login /></Route>
+        <Route path='/register'><Register /></Route>
+      </Switch>
     </div>
   );
 }
