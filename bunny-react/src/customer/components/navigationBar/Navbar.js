a99c93dda35a211f09226c5f3955da85486430e9
liqinzhang98 <103245879+liqinzhang98@users.noreply.github.com>
2024-05-24 16:21:54 +1000

    updated nav

--- bunny-react/src/customer/components/navigationBar/Navbar.js
+++ bunny-react/src/customer/components/navigationBar/Navbar.js
@@ -1,27 +1,41 @@
 import React, { useContext, useRef, useState } from 'react'
 import { Link } from 'react-router-dom';
 import './Navbar.css'; 
-import cart_icon from './cart_icon.png'
+import burger_icon from '../Assets/burger_icon.png';
+import nav_dropdown from '../Assets/nav_dropdown.png'
+import cart_icon from '../Assets/cart_icon.png'
+
 
 function NavigationBar() {
 
   const [menu,setMenu] = useState("shop");
 
+  const [showMenu, setShowMenu] = useState(false);
+
+  const menuRef = useRef();
+
+  const dropdown_toggle = (e) => {
+    menuRef.current.classList.toggle('nav-menu-visible');
+    e.target.classList.toggle('open');
+  }
+
 
   return (
     <div class="navbar">
-    <a class="nav-logo" href="/home">Navbar</a>
-      <div class="nav-menu">
+      <a class="nav-logo" href="/home">Navbar</a>
+      {/* <img src={burger_icon} alt="Menu" className="burger-icon" onClick={toggleMenu} /> */}
+      <img onClick={dropdown_toggle} className='nav-dropdown' src={nav_dropdown} alt="" />
+      <ul className={`nav-menu ${showMenu ? 'show' : ''}`}>
         <li onClick={()=>{setMenu("TCG")}} href="#">TCG </li>
         <li onClick={()=>{setMenu("Accessories")}} href="#">Accessories </li>
-      </div>
+      </ul>
       <div class="nav-login-register-cart">
         <Link to='/login'><button>Login</button></Link>
         <Link to="/register"><button>Register</button></Link>
         <Link class="nav-cart"to='/cart'><img src={cart_icon} alt="" /></Link>
         <div className="nav-cart-count">0</div> 
       </div>
-</div>
+    </div>
   );
 }
 
