12d92716b968b207a54efce754bcadc4ff131a57
liqinzhang98 <103245879+liqinzhang98@users.noreply.github.com>
2024-05-25 12:11:17 +1000

    Update Navbar.js

--- bunny-react/src/customer/components/navigationBar/Navbar.js
+++ bunny-react/src/customer/components/navigationBar/Navbar.js
@@ -25,13 +25,14 @@ function NavigationBar() {
       <img onClick={dropdown_toggle} className='nav-dropdown' src={nav_dropdown} alt="" />
       <ul className={`nav-menu ${showMenu ? 'show' : ''}`}>
         <li onClick={()=>{setMenu("TCG")}} href="#">TCG </li>
+        <li onClick={()=>{setMenu("BoardGames")}} href="#">BoardGames </li>
         <li onClick={()=>{setMenu("Accessories")}} href="#">Accessories </li>
       </ul>
       <div class="nav-login-register-cart">
         <Link to='/login'><button>Login</button></Link>
         <Link to="/register"><button>Register</button></Link>
         <Link class="nav-cart"to='/cart'><img src={cart_icon} alt="" /></Link>
-        <div className="nav-cart-count">0</div> 
+        <div className="nav-cart-count">2</div> 
       </div>
     </div>
   );
